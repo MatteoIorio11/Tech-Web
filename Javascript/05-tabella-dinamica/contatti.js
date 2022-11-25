0a0c86e329ea78eb78c974b010f387d7908f7e23
Matteo Iorio - matteo.iorio2@studio.unibo.it <matteo.iorio2@studio.unibo.it>
2022-11-25 12:40:08 +0100

    finish of the fifth excercise

--- Javascript/05-tabella-dinamica/contatti.js
+++ Javascript/05-tabella-dinamica/contatti.js
@@ -12,3 +12,25 @@ const datiTabella = [{
 function stringaToID(stringa){
     return stringa.toLowerCase().replace(/[^a-zA-Z]/g, "");
 }
+const table = document.querySelector("table");
+let content = "";
+Object.keys(datiTabella[0]).forEach(col => {
+    content += `<th id="${col.toLocaleLowerCase()}">${col}</th>`;
+});
+table.innerHTML = "<tr>" + content + "</tr>";
+content = "";
+datiTabella.forEach(element => {
+    content +="<tr>"
+    const id = stringaToID(element["Autore"]);
+    Object.keys(element).forEach(tag =>{
+        
+        if(tag === "Autore"){
+            content +=`<th id="${id}">${element[tag]}</th>`
+        }else{
+            content +=`<td headers="${tag.toLocaleLowerCase()} ${id} ">${element[tag]}</th>`
+        }
+    });
+    content += "</tr>";
+});
+table.innerHTML += content;
+
