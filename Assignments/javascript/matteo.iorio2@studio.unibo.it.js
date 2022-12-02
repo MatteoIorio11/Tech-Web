b70ffb87f6995aaf9569152e096db1e6a9297ae3
Matteo <matteo.iorio01@gmail.com>
2022-12-02 16:07:40 +0100

    new assignment

--- Assignments/javascript/matteo.iorio2@studio.unibo.it.js
+++ Assignments/javascript/matteo.iorio2@studio.unibo.it.js
@@ -1,13 +1,17 @@
 const CURRENT = "current";
 const img_tags = document.querySelectorAll("img");
-const img_lenght = img_tags.length;
+const img_length = img_tags.length;
 let current_index  = 0;
 img_tags[current_index].classList.add(CURRENT);
-//PUNTO 0
-//Al caricamento della pagina vengano nascoste tutte le immagini, eccetto le prime due.
-//Alla prima immagine deve essere inoltre aggiunta la classe "current". 
-for(let i = 2; i < img_tags.length; i++){
-    img_tags[i].style.display = 'none';
+coverAll();
+
+function coverAll(){
+    //PUNTO 0
+    //Al caricamento della pagina vengano nascoste tutte le immagini, eccetto le prime due.
+    //Alla prima immagine deve essere inoltre aggiunta la classe "current". 
+    for(let i = 2; i < img_length; i++){
+        img_tags[i].style.display = 'none';
+    }    
 }
 
 function handler(index){
@@ -19,6 +23,9 @@ function handler(index){
         //In caso contrario, invece, bisognerà aggiungere la classe current, rimuovendola da altre immagini
         img_tags[current_index].classList.remove(CURRENT);
         img_tags[index].classList.add(CURRENT);
+        //PUNTO 3
+        //Successivamente, andranno opportunamente nascoste e visualizzate le immagini in modo
+        // che siano visibili: l’immagine con classe current, l’eventuale immagine prima e l’eventuale immagine dopo.
         coverNeighbors(index);
         current_index = index;
         showNeighbors();
@@ -26,29 +33,31 @@ function handler(index){
 
 }
 
-//PUNTO 3
-//Successivamente, andranno opportunamente nascoste e visualizzate le immagini in modo
-// che siano visibili: l’immagine con classe current, l’eventuale immagine prima e l’eventuale immagine dopo.
+function setStyle(index, style=''){
+    img_tags[index].style.display = style;
+}
+
 
 function coverNeighbors(nw_curr){
     if(current_index-1 >= 0 && current_index-1 !== nw_curr){
-        img_tags[current_index-1].style.display = 'none';
+        setStyle(current_index-1, 'none');
     }
-    if(current_index+1 < img_lenght && current_index+1 !== nw_curr){
-        img_tags[current_index+1].style.display = 'none';
+    if(current_index+1 < img_length && current_index+1 !== nw_curr){
+        setStyle(current_index+1, 'none');
     }
 }
 
 function showNeighbors(){
     if(current_index-1 >= 0){
-        img_tags[current_index-1].style.display = '';
+        setStyle(current_index-1);
     }
-    if(current_index+1 < img_lenght){
-        img_tags[current_index+1].style.display = '';
+    if(current_index+1 < img_length){
+        setStyle(current_index+1);
     }
 }
 
-for(let i = 0; i < img_lenght; i++){
+//Adding the listeners
+for(let i = 0; i < img_length; i++){
     img_tags[i].addEventListener("click", event => {
         handler(i);
     });
